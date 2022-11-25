c00fe76dcd07718dc5f79d47cc96c10c5b491938
TaranAleksandr <108215188+TaranAleksandr@users.noreply.github.com>
2022-11-25 22:43:24 +0300

    add iframe scale and stoping video after close modal

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const modalWindow = document.querySelector('.modal');
-const buttonsModal = document.querySelectorAll('.button-play');const iFrame = document.querySelector('iframe');
+const buttonsModal = document.querySelectorAll('.button-play');
+const iframe = document.querySelector('iframe');
 
 buttonsModal.forEach((item, i) =>{
   item.addEventListener('click', () => {
@@ -9,5 +10,7 @@ buttonsModal.forEach((item, i) =>{
 
 modalWindow.addEventListener('click', () => {
   modalWindow.classList.remove('active');
+  iframe.removeAttribute('src');
+  iframe.setAttribute('src', 'https://www.youtube.com/embed/99Qy0SSI1Uw');
 });
 
